2cbc7fcc76e8ee553a5eb4fba705de0dfca64d89
Bassel Mostafa <bassel.ahmed97@eng-st.cu.edu.eg>
2017-12-20 00:42:16 +0200

    Added hard coded cards and user class still error in parsing json

--- app/home/home.component.ts
+++ app/home/home.component.ts
@@ -4,12 +4,16 @@ import { RadSideDrawerComponent } from "nativescript-pro-ui/sidedrawer/angular";
 import { getString, setString } from "tns-core-modules/application-settings/application-settings";
 import { RouterExtensions, PageRoute } from "nativescript-angular/router";
 
+import * as elementRegistryModule from 'nativescript-angular/element-registry';
+elementRegistryModule.registerElement("CardView", () => require("nativescript-cardview").CardView);
 
+import { User } from "./shared/classes/user";
 
 @Component({
     selector: "Home",
     moduleId: module.id,
-    templateUrl: "./home.component.html"
+    templateUrl: "./home.component.html",
+    styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit{
     /* ***********************************************************
@@ -20,17 +24,27 @@ export class HomeComponent implements OnInit{
 
     private _sideDrawerTransition: DrawerTransitionBase;
 
+    public user: User;
+
     /* ***********************************************************
     * Use the sideDrawerTransition property to change the open/close animation of the drawer.
     *************************************************************/
     ngOnInit(): void {
         this._sideDrawerTransition = new SlideInOnTopTransition();
-        console.log(getString("userdata","none"))
+        console.log(getString("userdata","none"));
         //setString("userdata","none")
         if(getString("userdata","none")=="none"){
             console.log("no user data found")
              this.routerExtensions.navigate(["home/login"]);            
          }
+
+        //let res: JSON;
+        //res = JSON.parse(getString("userdata","none"));
+
+        //this.user = new User(res.json().email ,res.json().password,res.json().password);
+         
+        //console.log( res.stringify.toString() );
+
     }
 
     get sideDrawerTransition(): DrawerTransitionBase {
--- app/home/shared/classes/user.ts
+++ app/home/shared/classes/user.ts
@@ -4,6 +4,9 @@ export class User{
     password: string;
     logo: string;
     passwordCheck: string;
+
+    name: string;
+
     constructor(mail: string,password: string,passwordCheck: string){
         this.password = password;
         this.email = mail;
